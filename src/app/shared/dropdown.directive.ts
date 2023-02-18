f07f40fdfffbb6895fed53f374e841d8e6f983c9
Andromeda <geraskin1997@gmail.com>
2023-02-18 09:00:52 +0200

    add dropdown directive

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,17 @@
+import {Directive, HostListener, Input, Renderer2} from "@angular/core";
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  isOpened = false;
+  @Input('appDropdown') dropdown!: HTMLElement;
+
+  constructor(private renderer: Renderer2) {
+  }
+  @HostListener('click') toggleOpen() {
+    this.isOpened = !this.isOpened;
+    const display = this.isOpened ? 'unset' : 'none';
+    this.renderer.setStyle(this.dropdown, 'display', display)
+  }
+}
